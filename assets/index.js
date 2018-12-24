98582a104a182db2c3824875bd232012941ed537
Andres De Jesus Cabrera <heyandres@air.attlocal.net>
2018-12-23 22:40:19 -0500

    added local server support

---
+++ assets/index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// Everything here runs before the document loads
+
+$(document).ready(function () {
+    // Everything here runs AFTER the document loads
+
+    let invoice_elements = $("invoice-body");
+
+    // invoice_elements.appendChild("tr");
+
+    //   let num = 0;
+    //   $(".counterbox").html(num);
+
+    //   function add() {
+    //     num++;
+    //     $(".counterbox").text(num);
+    //   }
+
+    //   function minus() {
+    //     num--;
+    //     $(".counterbox").text(num);
+    //   }
+
+    //   function reset() {
+    //     num = 0;
+    //     $(".counterbox").text(num);
+    //   }
+
+    //   $("#addBtn").click(add);
+    //   $("#minusBtn").click(minus);
+    //   $("#resetBtn").click(reset);
+});
---
+++ main.py
@@ -0,0 +1,7 @@
+grid = []
+
+for i in range(5):
+    for z in range(5):
+        grid.append(str(i)+":"+str(z))
+
+print(grid)
